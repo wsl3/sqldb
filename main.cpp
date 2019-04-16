e38c7bf97f44869606358ac107e35a13e92aa329
wsl3 <2350622075@qq.com>
2019-04-16 14:49:08 +0800

    update

--- main.cpp
+++ main.cpp
@@ -6,14 +6,40 @@
 using namespace std;
 
 int main() {
-    std::string s = "wsl";
-    DBMap m = DBMap(2);
-    if(m.ht->entrys[0] == nullptr){
-        cout<<"1"<<endl;
+    string s = "wsl";
+    string s1 = "wjl";
+
+
+    DBMap m = DBMap(8);
+
+    DBString* v1 = new DBString(s);
+    DBList* v2 = new DBList();
+
+    v2->append(s);
+    v2->appendl(s1);
+
+    m.insert(s, v1);
+    m.insert(s1, v2);
+    m.insert("1", v1);
+    m.insert("2", v1);
+    m.insert("2", v1);
+    m.insert("2", v1);
+    m.insert("3", v1);
+
+    for(int i=0;i<m.ht->size;i++){
+        if(m.ht->entrys[i] == nullptr){
+            cout<<i<<"---> NULL"<<endl;
+        }else{
+            cout<<i;
+            auto *p=m.ht->entrys[i];
+            while(p != nullptr){
+                cout<<"---> "<<"( "<<p->key->buff<<", "<<p->value->type<<" )";
+                p = p->next;
+            }
+            cout<<endl;
+        }
     }
-    if(m.ht->entrys[1] == nullptr){
-        cout<<"2"<<endl;
-    }
+
     cout << endl;
     return 0;
 }
