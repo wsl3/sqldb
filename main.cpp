2255266db254e858834f238b53de57f8fbcd6251
wsl3 <2350622075@qq.com>
2019-04-18 23:12:03 +0800

    sqldb-cli

--- main.cpp
+++ main.cpp
@@ -2,44 +2,58 @@
 #include "src/dstring.h"
 #include"src/dlist.h"
 #include "src/dmap.h"
+#include "sqlClick/sqlcli.h"
 
 using namespace std;
 
 int main() {
 
-    DBMap* p = new DBMap(10);
+//    DBMap* p = new DBMap(8);
+//
+//    DBString* v1 = new DBString("wsl");
+//    DBList* v2 = new  DBList();
+//    v2->appendl("123");
+//
+//    DBMap* v3 = new DBMap();
+//
+//    p->insert("1", v1);
+//    p->insert("2", v2);
+//    p->insert("3", v3);
+//    p->insert("4", v3);
+//    p->insert("5", v3);
+//    p->insert("6", v3);
+//    p->insert("7", v3);
+//
+//    p->keys();
+//    cout<<endl;
+//    p->traversal();
+//    for(int i=0;i<p->ht->size;i++){
+//        if(p->ht->entrys[i] != nullptr){
+//            cout<<p->ht->entrys[i]->value->values()<<endl;
+//        }
+//    }
+//
+//    DBMap m = *p;
+//    for(int i=0;i<m.ht->size;i++){
+//        if(m.ht->entrys[i] == nullptr){
+//            cout<<i<<"---> NULL"<<endl;
+//        }else{
+//            cout<<i;
+//            auto *p=m.ht->entrys[i];
+//            while(p != nullptr){
+//                cout<<"---> "<<"( "<<p->key->buff<<", "<<p->value->type<<" )";
+//                p = p->next;
+//            }
+//            cout<<endl;
+//        }
+//    }
 
-    DBString* v1 = new DBString("wsl");
-    DBList* v2 = new  DBList();
-    v2->appendl("123");
+    vector<string> test;
+    test = readCommand();
 
-    DBMap* v3 = new DBMap();
 
-    p->insert("1", v1);
-    p->insert("2", v2);
-    p->insert("3", v3);
-
-    for(int i=0;i<p->ht->size;i++){
-        if(p->ht->entrys[i] != nullptr){
-            cout<<p->ht->entrys[i]->value->values()<<endl;
-        }
+    for(unsigned long i=0;i<test.size();i++){
+        cout<<test[i]<<endl;
     }
-
-    DBMap m = *p;
-    for(int i=0;i<m.ht->size;i++){
-        if(m.ht->entrys[i] == nullptr){
-            cout<<i<<"---> NULL"<<endl;
-        }else{
-            cout<<i;
-            auto *p=m.ht->entrys[i];
-            while(p != nullptr){
-                cout<<"---> "<<"( "<<p->key->buff<<", "<<p->value->type<<" )";
-                p = p->next;
-            }
-            cout<<endl;
-        }
-    }
-
-    cout << endl;
     return 0;
 }
--- sqlClick/sqlcli.h
+++ sqlClick/sqlcli.h
@@ -5,4 +5,60 @@
 #ifndef SQLDB_SQLCLI_H
 #define SQLDB_SQLCLI_H
 
+#include <vector>
+#include <string>
+#include "../error/excptions.h"
+
+// string ---> tokens
+void lexer(std::string str, std::vector<std::string> &tokens) {
+
+    try {
+        unsigned long length = str.length();
+        std::string temp;
+        for (unsigned long first = 0, last = 0; last < length;) {
+            //保证 first,last
+            if (str[first] == ' ' && str[last] == ' ') {
+                first++;
+                last++;
+                continue;
+            }
+
+            if(str[first]=='\"'){
+                while (last < length && str[++last] != '\"') {}
+                if(last==length){
+                    throw LexerException();
+                }
+                first++;
+            }else{
+                while (last < length && str[++last] != '\"' && str[last] != ' ') {}
+                if(last<length && str[last]=='\"'){
+                    throw LexerException();
+                }
+            }
+            temp = str.substr(first, last-first);
+            tokens.push_back(temp);
+            last++;
+            first = last;
+        }
+    } catch (LexerException &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+
+}
+
+
+//从命令行读取 string, 返回 tokens
+std::vector<std::string> readCommand() {
+
+    std::string str;
+    std::vector<std::string> tokens;
+    std::cout<<"sqldb>> ";
+    std::getline(std::cin, str);
+
+    lexer(str, tokens);
+    return tokens;
+}
+
+
 #endif //SQLDB_SQLCLI_H
