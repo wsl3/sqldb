9185c173b6bef8a6537bbbc2eb2628042a4c2298
wsl3 <2350622075@qq.com>
2019-04-18 23:09:26 +0800

    自定义exception

--- error/excptions.h
+++ error/excptions.h
@@ -5,4 +5,14 @@
 #ifndef SQLDB_EXCPTIONS_H
 #define SQLDB_EXCPTIONS_H
 
+#include <exception>
+
+class LexerException : public std::exception {
+public:
+
+    const char *what() const throw() {
+        return "LexError: can't find another \"";
+    }
+};
+
 #endif //SQLDB_EXCPTIONS_H
